23e9439200ef527571f3b30bf7a62adaa3d9862b
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2016-05-23 00:35:06 +0200

    Add separate router

---
+++ src/router.js
@@ -0,0 +1,25 @@
+import Router from 'vue-router';
+import TodoApp from 'components/todo';
+import Home from 'components/home';
+import NotFound from 'components/errors/404';
+
+// Router options
+var router = new Router({
+    hashbang: false,
+    root: '/'
+});
+
+// Set routes
+router.map({
+    '*': {
+      component: NotFound
+    },
+    '/': {
+      component: Home
+    },
+    '/todo': {
+      component: TodoApp
+    }
+});
+
+export default router;
