7c3fc563e98b2ae8fa02373bdc5261fa159fe270
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2016-05-23 00:34:39 +0200

    Add firebase init

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDSytBl320K7q0SEGAetA85cxHXygNKQYE",
+  authDomain: "vuex-todos.firebaseapp.com",
+  databaseURL: "https://vuex-todos.firebaseio.com",
+  storageBucket: "",
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
