b717b02597f1f0f01f1c7d51156248f77a2a1a4a
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2016-05-23 00:30:21 +0200

    Add input error component

---
+++ src/components/errors/errors.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="errors">
+    <p v-for="error in errors">{{error}}</p>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue';
+
+  export default {
+    props: {
+      errors: Array
+    }
+  }
+</script>
+
+<style>
+  .errors {
+    font-size: 12px;
+    color: red;
+  }
+</style>
