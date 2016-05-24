2c25bda52d015692eb94215815a57b1026a75f48
Philipp Gfeller <tuelsch@gmail.com>
2016-05-24 09:28:52 +0200

    Change active class for links

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import Router from 'vue-router';
 import TodoApp from 'components/todo';
 import Home from 'components/home';
 import NotFound from 'components/errors/404';
+import SignUp from 'components/authentication/signup';
 
 // Router options
 var router = new Router({
     hashbang: false,
-    root: '/'
+    root: '/',
+    linkActiveClass: 'active'
 });
 
 // Set routes
