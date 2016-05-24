696d53a522c223fcee08006ab75d410d638b4deb
Philipp Gfeller <tuelsch@gmail.com>
2016-05-24 09:29:52 +0200

    Import vue touch and validator

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import App from 'src/App';
 import router from 'src/router';
+import VueTouch from 'vue-touch';
+import validator from 'vue-validator';
+
+Vue.use(VueTouch);
 
 // Mount app with router
 router.start(App, '#vue-app');
