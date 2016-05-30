7b1ca62ef0d6d44a6d12289cf2894d69fd5907d8
Philipp Gfeller <tuelsch@gmail.com>
2016-05-30 11:16:12 +0200

    Update signup form to support validation and actually singing up people

--- src/components/authentication/signup.vue
+++ src/components/authentication/signup.vue
@@ -1,68 +1,113 @@
 <template>
-    <validator name="sign_up">
-        <form novalidate class="container">
-            <div class="card-panel">
-                <div class="row">
-                    <div class="col s12">
-                        <div class="input-field">
+    <div class="center-center">
+        <validator name="sign_up">
+            <form novalidate class="container" action="" method="post">
+                <div class="card-panel">
+                    <div class="row">
+                        <div class="input-field col s12">
                             <input
                                 type="email"
                                 id="username"
                                 name="username"
-                                v-validate:username="['required', 'email']">
-                            <label for="username">Email</label>
+                                v-model="username"
+                                v-validate:username="{required:true, email:true}">
+
+                            <div class="errors" v-if="$sign_up.username.dirty">
+                                <p class="error" v-if="$sign_up.username.required">Username required</p>
+                                <p class="error" v-if="$sign_up.username.email">Use your email</p>
+                            </div>
+
+                            <label for="username">Username (Email)</label>
                         </div>
                     </div>
-                </div>
 
-                <div class="errors">
-                    <p v-if="$sign_up.username.required">Username required</p>
-                    <p v-if="$sign_up.username.email">Use email</p>
-                </div>
-
-                <div class="row">
-                    <div class="col s12">
-                        <div class="input-field">
+                    <div class="row">
+                        <div class="input-field col s12">
                             <input
                                 type="password"
                                 id="password"
                                 name="password"
-                                v-validate:password="['required']">
+                                v-model="password"
+                                v-validate:password="{required:true}">
+
+                            <div class="errors" v-if="$sign_up.password.dirty">
+                                <p v-if="$sign_up.password.required">This field is required.</p>
+                            </div>
+
                             <label for="password">Password</label>
                         </div>
                     </div>
-                </div>
 
-                <div class="row">
-                    <div class="col s12">
-                        <div class="input-field">
+                    <div class="row">
+                        <div class="input-field col s12">
                             <input
                                 type="password"
                                 id="match_password"
-                                name="match_password">
+                                v-model="match_password"
+                                name="match_password"
+                                v-validate:match_password="{
+                                    required:true,
+                                    match:{rule:password},
+                                    minlength:6
+                                }">
+                            
+                            <div v-if="$sign_up.match_password.dirty" class="errors">
+                                <p v-if="$sign_up.match_password.required">This field is required.</p>
+                                <p v-if="$sign_up.match_password.match">Both passwords must match.</p>
+                            </div>
+
                             <label for="match_password">Repeat password</label>
                         </div>
                     </div>
-                </div>
 
-                <p class="right-align">
-                    <waves :color="'white'">
-                        <button type="submit" class="btn teal">Sign in</button>
-                    </waves>
-                </p>
-            </div>
-        </form>
-    </validator>
+                    <errors :errors="errors"></errors>
+
+                    <p class="right-align">
+                        <button :disabled="$sign_up.invalid || loading" @click="submit" type="submit" class="btn teal waves-effect waves-light">Sign up</button>
+                    </p>
+                </div>
+            </form>
+        </validator>
+    </div>
 </template>
 
 <script>
     import validator from 'vue-validator';
-    import waves from 'vue-materialize/waves';
+    import firebase from 'firebase';
+    import errors from 'components/errors/errors';
+    import router from 'src/router';
 
     export default {
+        data () {
+            return {
+                username: '',
+                password: '',
+                match_password: '',
+                errors: [],
+                loading: false
+            }
+        },
+        methods: {
+            submit () {
+                event.preventDefault();
+                this.loading = true;
+                firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(this.username, this.password)
+                    .then(user => {
+                        this.errors = [];
+                        this.loading = false;
+                        router.go('/signup-success');
+                    })
+                    .catch(err => {
+                        this.errors.push(err.message);
+                        this.loading = false;
+                    });
+            }
+        },
         components: {
-            waves,
-            validator
+            validator,
+            errors
         }
     }
 </script>
