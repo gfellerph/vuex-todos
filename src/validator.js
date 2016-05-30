59f2ca29086df1c1fc508280e3d26f541b134791
Philipp Gfeller <tuelsch@gmail.com>
2016-05-30 11:18:42 +0200

    Add custom match validator

--- src/validator.js
+++ src/validator.js
@@ -3,3 +3,14 @@ import Vue from 'vue';
 Vue.validator('email', function (val) {
   return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(val)
 });
+
+/**
+ * See if value matches the ruled value,
+ * for password confirm and such
+ * @param  {var} 'val'  Input value
+ * @param  {var} 'rule' Compare value
+ * @return {Boolean} Validation result
+ */
+Vue.validator('match', (val, rule) => {
+	return val === rule;
+});
