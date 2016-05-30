27b6bac04e4015568bf770418bf4bf00a0fa4a33
Philipp Gfeller <tuelsch@gmail.com>
2016-05-30 15:22:16 +0200

    Use module option to merge store props

--- src/store.js
+++ src/store.js
@@ -4,14 +4,9 @@ import todo from 'components/todo/todo-store';
 import auth from 'components/authentication/authentication-store';
 
 export default new Vuex.Store({
-	state: Object.assign(
-		{},
-		{todo: todo.state},
-		{auth: auth.state}
-	),
-	mutations: Object.assign(
-		{},
-		todo.mutations,
-		auth.mutations
-	)
+	strict: true,
+	modules: {
+		todo,
+		auth
+	}
 });
