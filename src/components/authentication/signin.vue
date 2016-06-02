dc0397a778435bcde14c016b526bca6287847e74
Philipp Gfeller <tuelsch@gmail.com>
2016-06-02 10:28:00 +0200

    Add loading indicator and fix stack overflow created by passing the firebase user directly to vuex

--- src/components/authentication/signin.vue
+++ src/components/authentication/signin.vue
@@ -1,6 +1,6 @@
 <template>
     <form novalidate class="container" action="" method="post">
-        <div class="card-panel">
+        <div class="card-panel login-form">
             <div class="row">
                 <div class="input-field col s12">
                     <input
@@ -28,8 +28,15 @@
             <errors :errors="errors"></errors>
 
             <p class="right-align">
-                <button :disabled="isEmpty" @click="login" type="submit" class="btn teal waves-effect waves-light">Login</button>
+                <button
+                    class="btn teal waves-effect waves-light"
+                    type="submit"
+                    :disabled="isEmpty || isLoading"
+                    @click="login"
+                >Login</button>
             </p>
+            
+            <spinner v-if="isLoading"></spinner>
         </div>
     </form>
 </template>
@@ -38,13 +45,16 @@
     import store from 'src/store';
     import firebase from 'src/firebase';
     import errors from 'components/errors/errors';
+    import User from 'models/User';
+    import spinner from 'components/shared/spinner';
 
 	export default {
 		data () {
 			return {
 				username: '',
 				password: '',
-                errors: []
+                errors: [],
+                isLoading: false
 			}
 		},
         computed: {
@@ -55,20 +65,38 @@
 		methods: {
 			login (event) {
 				event.preventDefault();
+                this.isLoading = true;
+
                 firebase
                     .auth()
                     .signInWithEmailAndPassword(this.username, this.password)
-                    .then(user => {
+                    .then(() => {
                         this.errors = [];
-                        store.dispatch('LOGIN', {user});
+                        this.isLoading = false;
+                        store.dispatch('LOGIN', {user: new User(firebase.auth().currentUser)});
                     })
                     .catch(err => {
+                        this.isLoading = false;
                         this.errors = [err.message];
                     });
 			}
 		},
         components: {
-            errors
+            errors,
+            spinner
         }
 	}
 </script>
+
+<style lang="scss">
+    .login-form {
+        position: relative;
+
+        .progress {
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            margin: 0;
+        }
+    }
+</style>
