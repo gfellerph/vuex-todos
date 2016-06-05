82ace127b6fbc04c23e7bdbc96603f4b5292f9f3
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2016-06-05 12:22:04 +0200

    Add profile view with sign out button

--- src/components/authentication/profile.vue
+++ src/components/authentication/profile.vue
@@ -1,3 +1,37 @@
 <template>
-	<h1>profile</h1>
+	<div class="profile grey-text text-darken-4">
+   <h2>{{user.email}}</h2>
+   <p class="grey-text text-lighten-2">{{user.uid}}</p>
+   <p class="right-align">
+     <button
+      class="pink waves-effect btn"
+      @click="signout"
+     >sign out</button>
+   </p>
+  </div>
 </template>
+
+<script>
+  import firebase from 'src/firebase';
+
+  export default {
+    methods: {
+      signout: () => {
+        firebase
+          .auth()
+          .signOut();
+      }
+    },
+    vuex: {
+      getters: {
+        user: state => state.auth.user
+      }
+    }
+  }
+</script>
+
+<style>
+  .profile {
+    padding: 0 2rem;
+  }
+</style>
