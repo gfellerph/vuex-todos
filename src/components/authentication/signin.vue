daab0c4008fe3b98be48fdf402b05c46a053d3a3
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2016-06-07 00:22:53 +0200

    Center position for signin form

--- src/components/authentication/signin.vue
+++ src/components/authentication/signin.vue
@@ -1,48 +1,51 @@
 <template>
-    <form novalidate class="container" action="" method="post">
-        <div class="login-form">
-            <div class="row">
-                <div class="input-field col s12">
-                    <input
-                        type="email"
-                        id="username"
-                        name="username"
-                        v-model="username">
+    <div class="center-center">
+        <form novalidate class="container card-panel" action="" method="post">
+            <h4>Sign in</h4>
+            <div class="login-form">
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input
+                            type="email"
+                            id="username"
+                            name="username"
+                            v-model="username">
 
-                    <label for="username">Username (Email)</label>
+                        <label for="username">Username (Email)</label>
+                    </div>
                 </div>
-            </div>
 
-            <div class="row">
-                <div class="input-field col s12">
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        v-model="password">
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            v-model="password">
 
-                    <label for="password">Password</label>
+                        <label for="password">Password</label>
+                    </div>
                 </div>
+
+                <errors :errors="errors"></errors>
+
+                <p class="right-align">
+                    <a
+                        v-link="'signup'"
+                        class="btn-flat waves-effect"
+                    >sign up</a>
+                    <button
+                        class="btn green waves-effect waves-light"
+                        type="submit"
+                        :disabled="isEmpty || isLoading"
+                        @click="login"
+                    >Login</button>
+                </p>
+                
+                <spinner v-if="isLoading"></spinner>
             </div>
-
-            <errors :errors="errors"></errors>
-
-            <p class="right-align">
-                <a
-                    v-link="'signup'"
-                    class="btn teal waves-effect"
-                >sign up</a>
-                <button
-                    class="btn teal waves-effect waves-light"
-                    type="submit"
-                    :disabled="isEmpty || isLoading"
-                    @click="login"
-                >Login</button>
-            </p>
-            
-            <spinner v-if="isLoading"></spinner>
-        </div>
-    </form>
+        </form>
+    </div>
 </template>
 
 <script>
