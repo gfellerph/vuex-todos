f359387e14f65769c2c62d1d0ccb24093fe7dc6c
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2016-06-07 00:23:04 +0200

    Add some content to signup success

--- src/components/authentication/signup-success.vue
+++ src/components/authentication/signup-success.vue
@@ -1,3 +1,11 @@
 <template>
-	<h1>signup success</h1>
+	<div class="center-center">
+   <div class="container card-panel">
+     <h3>Thanks for signing up</h3>
+     <p>You are now ready to start doing things.</p>
+     <p class="right-align">
+       <a class="btn green waves-effect" v-link="'todo'">todos</a>
+     </p>
+   </div>
+  </div>
 </template>
