507fe72450eb7af162d0a5029d0cda91ee6f4390
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2016-06-07 00:23:38 +0200

    Cleanup / add title

--- src/components/authentication/signup.vue
+++ src/components/authentication/signup.vue
@@ -1,80 +1,80 @@
 <template>
     <div class="center-center">
         <validator name="sign_up">
-            <form novalidate class="container" action="" method="post">
-                <div class="card-panel">
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input
-                                type="email"
-                                id="username"
-                                name="username"
-                                v-model="username"
-                                v-validate:username="{required:true, email:true}">
+            <form novalidate class="container card-panel" action="" method="post">
+                <h4>Sign up</h4>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input
+                            type="email"
+                            id="username"
+                            name="username"
+                            v-model="username"
+                            v-validate:username="{required:true, email:true}"
+                        />
 
-                            <div class="errors" v-if="$sign_up.username.dirty">
-                                <p class="error" v-if="$sign_up.username.required">Username required</p>
-                                <p class="error" v-if="$sign_up.username.email">Use your email</p>
-                            </div>
-
-                            <label for="username">Username (Email)</label>
+                        <div class="errors" v-if="$sign_up.username.dirty">
+                            <p class="error" v-if="$sign_up.username.required">Username required</p>
+                            <p class="error" v-if="$sign_up.username.email">Use your email</p>
                         </div>
+
+                        <label for="username">Username (Email)</label>
                     </div>
-
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input
-                                type="password"
-                                id="password"
-                                name="password"
-                                v-model="password"
-                                v-validate:password="{required:true}">
-
-                            <div class="errors" v-if="$sign_up.password.dirty">
-                                <p v-if="$sign_up.password.required">This field is required.</p>
-                            </div>
-
-                            <label for="password">Password</label>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input
-                                type="password"
-                                id="match_password"
-                                v-model="match_password"
-                                name="match_password"
-                                v-validate:match_password="{
-                                    required:true,
-                                    match:{rule:password},
-                                    minlength:6
-                                }">
-                            
-                            <div v-if="$sign_up.match_password.dirty" class="errors">
-                                <p v-if="$sign_up.match_password.required">This field is required.</p>
-                                <p v-if="$sign_up.match_password.match">Both passwords must match.</p>
-                            </div>
-
-                            <label for="match_password">Repeat password</label>
-                        </div>
-                    </div>
-
-                    <errors :errors="errors"></errors>
-
-                    <p class="right-align">
-                        <a
-                            v-link="'signin'"
-                            class="btn teal waves-effect"
-                        >sign in</a>
-                        <button
-                            :disabled="$sign_up.invalid || loading"
-                            @click="submit"
-                            type="submit"
-                            class="btn teal waves-effect waves-light"
-                        >Sign up</button>
-                    </p>
                 </div>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            v-model="password"
+                            v-validate:password="{required:true}">
+
+                        <div class="errors" v-if="$sign_up.password.dirty">
+                            <p v-if="$sign_up.password.required">This field is required.</p>
+                        </div>
+
+                        <label for="password">Password</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input
+                            type="password"
+                            id="match_password"
+                            v-model="match_password"
+                            name="match_password"
+                            v-validate:match_password="{
+                                required:true,
+                                match:{rule:password},
+                                minlength:6
+                            }">
+                        
+                        <div v-if="$sign_up.match_password.dirty" class="errors">
+                            <p v-if="$sign_up.match_password.required">This field is required.</p>
+                            <p v-if="$sign_up.match_password.match">Both passwords must match.</p>
+                        </div>
+
+                        <label for="match_password">Repeat password</label>
+                    </div>
+                </div>
+
+                <errors :errors="errors"></errors>
+
+                <p class="right-align">
+                    <a
+                        v-link="'signin'"
+                        class="btn-flat waves-effect"
+                    >sign in</a>
+                    <button
+                        :disabled="$sign_up.invalid || loading"
+                        @click="submit"
+                        type="submit"
+                        class="btn green waves-effect waves-light"
+                    >Sign up</button>
+                </p>
             </form>
         </validator>
     </div>
