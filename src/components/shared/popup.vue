b0a54e0fc07880b93cd9603892b84abc81b1314b
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2016-06-07 00:24:09 +0200

    Apply card-panel styles to popup content

--- src/components/shared/popup.vue
+++ src/components/shared/popup.vue
@@ -3,7 +3,7 @@
 		<div class="popup-toggler" @click="togglePopup">
 			<slot name="label"></slot>
 		</div>
-		<div class="popup-content" data-anchor="{{anchor}}">
+		<div class="popup-content card-panel" data-anchor="{{anchor}}">
 			<slot name="content"></slot>
 		</div>
 	</div>
@@ -43,14 +43,14 @@
 	.popup-content {
 		position: absolute;
 		left: 0;
+		margin: 0;
 		background: white;
 		min-width: 250px;
 		opacity: 0;
 		visibility: hidden;
 		transform: rotateX(-70deg);
 		transform-origin: top center;
-		box-shadow: 0 0px 0px rgba(0,0,0,0);
-		transition: all 250ms, box-shadow 250ms, visibility 250ms 250ms;
+		transition: all 250ms, visibility 250ms 250ms;
 
 		&[data-anchor="right"] {
 			right: 0;
@@ -61,8 +61,7 @@
 			opacity: 1;
 			visibility: visible;
 			transform: rotateX(0);
-			box-shadow: 0 10px 15px rgba(0,0,0,0.3);
-			transition: all 200ms ease-out, box-shadow 250ms, visibility 0s 0s;
+			transition: all 200ms ease-out, visibility 0s 0s;
 		}
 	}
 
