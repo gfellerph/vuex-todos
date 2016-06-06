c845d04c9ae1dea0929571394521a75855c9ad24
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2016-06-07 00:25:12 +0200

    Include delete button and some styles

--- src/components/todo/todo-item.vue
+++ src/components/todo/todo-item.vue
@@ -1,32 +1,85 @@
 <template>
-  <span
-    v-on:click="toggleTodo"
-    v-bind:class="{toggled:todo.toggled}"
-    class="todo-item card-panel"
-  >{{todo.text}}</span>
+  <div
+    class="card-panel todo-item"
+    @click="toggleTodo"
+  >
+    <p
+      v-bind:class="{toggled:todo.toggled}"
+      class="todo-text"
+    >{{todo.text}}</p>
+    <button
+      class="todo-delete"
+      @click="deleteTodo"
+    >
+      <i class="material-icons">delete</i>
+    </button>
+  </div>
 </template>
 
 <script>
-  import Vue from 'vue';
+  import firebase from 'src/firebase';
 
-  export default Vue.extend({
+  export default {
     props: {
       todo: Object
     },
-    vuex: {
-      actions: {
-        toggleTodo: function ({dispatch}) {
-          dispatch('TOGGLE_TODO', {todo: this.todo});
-        }
+    methods: {
+      toggleTodo () {
+        firebase
+          .database()
+          .ref(`todos/${this.todo.id}`)
+          .set(Object.assign({}, this.todo, {
+            toggled: !this.todo.toggled
+          }));
+      },
+      deleteTodo () {
+        firebase
+          .database()
+          .ref(`todos/${this.todo.id}`)
+          .remove();
       }
     }
-  });
+  };
 </script>
 
 <style lang="scss">
   .todo-item {
     cursor: pointer;
-    display: block;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .todo-delete {
+    border: none;
+    background: none;
+    box-shadow: none;
+    outline: none;
+    opacity: 0;
+    color: #e0e0e0;
+    transition: opacity 500ms, color 500ms;
+
+    &:hover,
+    &:active,
+    &:focus {
+      background: none;
+      border: none;
+      outline: none;
+      color: #d32f2f;
+    }
+
+    .todo-item:hover & {
+      opacity: 1;
+    }
+
+    > i {
+      display: block;
+    }
+  }
+
+  .todo-text {
+    margin: 0;
+    width: 100%;
   }
 
   .toggled {
