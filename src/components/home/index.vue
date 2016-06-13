49b12e3918dc2ca68280d690f5764d9100c1e8dd
Philipp Gfeller <tuelsch@gmail.com>
2016-06-13 09:56:01 +0200

    Add some home content

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,3 +1,12 @@
 <template>
-  <p class="container">This is Sparta!</p>
+  <img class="sparta" src="https://media.giphy.com/media/tM6OJx2j3VFOU/giphy.gif" alt="">
 </template>
+
+<style>
+	.sparta {
+		display: block;
+		max-height: 50vh;
+		max-width: 70vw;
+		margin: 50px auto;
+	}
+</style>
