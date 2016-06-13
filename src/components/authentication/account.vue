fac488a26b64f96f185619a5177f6598b269708c
Philipp Gfeller <tuelsch@gmail.com>
2016-06-13 09:54:03 +0200

    Use profile image component

--- src/components/authentication/account.vue
+++ src/components/authentication/account.vue
@@ -1,36 +1,27 @@
 <template>
 	<div class="account">
 		<popup v-if="loggedIn" class="right" anchor="right">
-			<img
-				slot="label"
-				class="circle account-image"
-				:src="gravatar"
-				alt="">
+		  <profile-image slot="label" class="account-image"></profile-image>
 		  <profile slot="content"></profile>
 		</popup>
 		<a
 			v-link="{path: '/signin'}"
 			v-if="!loggedIn"
-			class="account-login btn green waves-effect waves-light"
-		>Login</a>
+			class="account-login btn cyan waves-effect waves-light"
+		>Sign in</a>
 	</div>
 </template>
 
 <script>
-	import md5 from 'md5';
 	import router from 'src/router';
 	import User from 'models/User';
 	import firebase from 'src/firebase';
 	import store from 'src/store';
 	import profile from 'components/authentication/profile';
+	import profileImage from 'components/authentication/profile-image';
 	import popup from 'components/shared/popup';
 
 	export default {
-	  computed: {
-	    gravatar () {
-	      return 'https://www.gravatar.com/avatar/' + md5(this.user.email);
-	    }
-	  },
 	  created () {
 
 	  	  // Listen to auth changes and dispatch accordingly
@@ -54,19 +45,20 @@
 	  },
 	  components: {
 	  	profile,
-	  	popup
+	  	popup,
+	  	profileImage
 	  }
 	}
 </script>
 
 <style>
+	.account-login {
+		margin-left: 14px;
+	}
+
 	.account-image {
 		display: block;
 		width: 50px;
 		margin: 7px 14px;
-	}
-
-	.account-login {
-		margin-left: 14px;
 	}
 </style>
