a12beda3e2dbe34fc2746af5ec46183d4759e5f3
Philipp Gfeller <tuelsch@gmail.com>
2016-06-13 09:55:44 +0200

    Add credits component

---
+++ src/components/shared/credits.vue
@@ -0,0 +1,28 @@
+<template>
+	<footer>
+		<p class="grey-text center-align">Created with</p>
+		<div class="footer-logos">
+			<a href="http://vuejs.org/" target="_blank">
+				<img src="~src/assets/logo.png" alt="vue js logo">
+			</a>
+			<i class="material-icons grey-text">add</i>
+			<a href="https://firebase.google.com/" target="_blank">
+				<img src="~src/assets/firebase.png" alt="firebase logo">
+			</a>
+		</div>
+	</footer>
+</template>
+
+<style lang="scss">
+	.footer-logos {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin: 20px 0;
+
+		img {
+			width: 42px;
+			margin: 0 20px;
+		}
+	}
+</style>
