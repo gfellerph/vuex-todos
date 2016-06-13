6768de24ec95274336800a54e0156a1584295d11
Philipp Gfeller <tuelsch@gmail.com>
2016-06-13 09:54:12 +0200

    Add profile image component

---
+++ src/components/authentication/profile-image.vue
@@ -0,0 +1,23 @@
+<template>
+	<img
+		class="circle"
+		:src="gravatar"
+		alt="">
+</template>
+
+<script>
+	import md5 from 'md5';
+
+	export default {
+	  computed: {
+	    gravatar () {
+	      return 'https://www.gravatar.com/avatar/' + md5(this.user.email);
+	    }
+	  },
+	  vuex: {
+	    getters: {
+	      user: state => state.auth.user
+	    }
+	  }
+	}
+</script>
